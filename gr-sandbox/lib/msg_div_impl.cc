e48203b517ebb2313a5c620130caca9fcc8ba112
tbickle <tbickle.matt@gmail.com>
2014-10-18 15:39:06 -0700

    .

--- gr-sandbox/lib/msg_div_impl.cc
+++ gr-sandbox/lib/msg_div_impl.cc
@@ -38,14 +38,14 @@ namespace gr {
 			/////// size determination //////
 			size_t len = pmt::length(vector);
 			size_t offset(0);
-			int reg_len = 5; // user defined
+			int reg_len = 5; 	// <--- USER DEFINED (size of division)
 			int last_len = int(len) % reg_len;
-			printf("last len=%i\n",last_len);
+			//printf("last len=%i\n",last_len);
 			offset = size_t(reg_len);
 			int nchunks;
-			if(last_len==0) nchunks = int(len)/reg_len;
-			else nchunks = int(len)/reg_len+last_len/last_len;
-			printf("# of chuncks=%i\n",nchunks);
+			if(last_len==0) nchunks=int(len)/reg_len;
+			else nchunks=int(len)/reg_len+last_len/last_len;
+			//printf("# of chuncks=%i\n",nchunks);
 			/////////////////////////////////
 
       			const uint8_t* d = (const uint8_t*) pmt::uniform_vector_elements(vector, offset);
@@ -55,24 +55,48 @@ namespace gr {
 			uint8_t* bytes_out;
 			pmt::pmt_t output;
 			pmt::pmt_t tvec;
+			pmt::pmt_t d_k1; pmt::pmt_t d_v1;
+			pmt::pmt_t d_k2; pmt::pmt_t d_v2;
+			bool last = 0;
 
 			// break up incoming vector
 			for(size_t ii=0; ii<nchunks; ii++) 
 			{
-				if(ii!=(nchunks-1)) { // not last chunk
+				// not last chunk
+				if(int(ii)<(nchunks-1)) { 	// not last chunk
 					tvec = pmt::init_u8vector(a,&d[ii*a]);
 					bytes_in = pmt::u8vector_elements(tvec, a);
 					bytes_out = (uint8_t*)volk_malloc(0 + a*sizeof(uint8_t), volk_get_alignment());
 					memcpy((void*)bytes_out, (const void*)bytes_in, a);
-					output = pmt::init_u8vector(a+0, bytes_out); 		// this copies the values from bytes_out into the u8vector
+					output = pmt::init_u8vector(a+0, bytes_out); 			// this copies the values from bytes_out into the u8vector
+					last = 0;
 				}
-				else { // last chunk
+				// last chunk
+				else if(last_len==0) {
+					tvec = pmt::init_u8vector(a,&d[ii*a]);
+					bytes_in = pmt::u8vector_elements(tvec, a);
+					bytes_out = (uint8_t*)volk_malloc(0 + a*sizeof(uint8_t), volk_get_alignment());
+					memcpy((void*)bytes_out, (const void*)bytes_in, a);
+					output = pmt::init_u8vector(a+0, bytes_out); 			// this copies the values from bytes_out into the u8vector
+					last = 1;
+				}
+				else
+				{
 					tvec = pmt::init_u8vector(b,&d[ii*a]);
 					bytes_in = pmt::u8vector_elements(tvec, b);
 					bytes_out = (uint8_t*)volk_malloc(0 + b*sizeof(uint8_t), volk_get_alignment());
 					memcpy((void*)bytes_out, (const void*)bytes_in, b);
-					output = pmt::init_u8vector(b+0, bytes_out); 		// this copies the values from bytes_out into the u8vector
+					output = pmt::init_u8vector(b+0, bytes_out); 			// this copies the values from bytes_out into the u8vector
+					last = 1;
 				}
+
+				////////// inject a key/value ////////
+				d_k1 = pmt::intern("pdu_id"); d_v1=pmt::from_double(double(ii));	// set d_k1 and d_v1
+				d_k2 = pmt::intern("last_flg"); d_v2=pmt::from_bool(last); 		// set d_k2 and d_v2
+				meta = pmt::dict_add(meta, d_k1, d_v1);
+				meta = pmt::dict_add(meta, d_k2, d_v2);
+				//////////////////////////////////////
+
 				// construct pdu
 				pmt::pmt_t msg_pair = pmt::cons(meta, output);
 				message_port_pub(d_out_port, msg_pair);
@@ -82,36 +106,4 @@ namespace gr {
 
   	} /* namespace sandbox */
 } /* namespace gr */
-
-/*
-			// inject a key/value
-			pmt::pmt_t d_k1; pmt::pmt_t d_v1;
-			pmt::pmt_t d_k2; pmt::pmt_t d_v2;
-			d_k1 = pmt::intern("pdu_id"); d_v1 = pmt::intern("1"); 			// set d_k1 and d_v1
-			bool last = pmt::is_eof_object(vector);
-			d_k2 = pmt::intern("last_flg"); d_v2 = pmt::intern("last"); 		// set d_k2 and d_v2
-      			meta = pmt::dict_add(meta, d_k1, d_v1);
-      			meta = pmt::dict_add(meta, d_k2, d_v2);
-*/
-/*
-      			const uint8_t* d = (const uint8_t*) pmt::uniform_vector_elements(vector, offset);
-      			for(size_t i=0; i<len; i+=16)
-			{
-        			printf("%04x: ", ((unsigned int)i));
-        			for(size_t j=i; j<std::min(i+16,len); j++)
-          			printf("%02x ",d[j] );
-        			std::cout << std::endl;
-      			}
-*/
-/*
-      			//for(size_t i=(a*ii); i<(a*ii+a); i+=16)
-			//{
-        			//printf("%04x: ", ((unsigned int)i));
-        			//for(size_t j=i+a*ii; j<std::min(i+a*ii+16,a); j++)
-          			//printf("%02x ", d[j]);
-        			//std::cout << std::endl;
-      			//}
-*/
-
-//construct a pdu (1 metadata pmt + 1 data pmt)
 
